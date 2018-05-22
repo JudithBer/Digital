7ba260b61dcce394b615ae08e3e3f79353d04ce1
Annika Keil <annika.keil95@gmail.com>
2018-05-22 14:19:16 +0200

    EmptyCoverException angelegt

---
+++ src/main/java/de/neemann/digital/analyse/espresso/exceptions/EmptyCoverException.java
@@ -0,0 +1,7 @@
+package de.neemann.digital.analyse.espresso.exceptions;
+
+public class EmptyCoverException extends Exception {
+    public EmptyCoverException(String msg) {
+        super(msg);
+    }
+}

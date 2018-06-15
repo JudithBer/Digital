ddbfff06bc8c25f7b4703d6f99d1923d9a659268
Annika Keil <annika_keil@yahoo.de>
2018-06-16 00:03:29 +0200

    Files verschoben

---
+++ src/test/java/de/neemann/digital/analyse/heuristic/espresso/EspressoTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2016 Helmut Neemann
+ * Use of this source code is governed by the GPL v3 license
+ * that can be found in the LICENSE file.
+ */
+package de.neemann.digital.analyse.heuristic.espresso;
+
+/**
+ * Tests for EspressoMatrix.java
+ */
+public class EspressoTest {
+
+    // TODO Test EspressoMinimizer at the end with some Test-BoolTables
+
+}
